f6f03db69be173feaa396313ac1a4047c78c0eaa
Daniel BC <danibeca@okazo.co>
2017-11-01 15:52:43 -0500

    Adding new levels

--- database/migrations/2017_10_16_130934_create_client_record.php
+++ database/migrations/2017_10_16_130934_create_client_record.php
@@ -22,6 +22,22 @@ class CreateClientRecord extends Migration
                     'redirect' => 'http://localhost',
                     'password_client' => 1,
                     'created_at' => '2017-10-22 01:34:38'
+                ),
+                array(
+                    'id'   => 2,
+                    'name' => 'quind-client',
+                    'secret' => 'PXEQA7HEEn4O8ZdAqqDUDcZxhE2OA41ZGovL3IRo',
+                    'redirect' => 'http://localhost',
+                    'password_client' => null,
+                    'created_at' => '2017-10-22 01:34:38'
+                ),
+                array(
+                    'id'   => 3,
+                    'name' => 'wanda-client',
+                    'secret' => 'iw6g3EWUSKMZi8K2L97WK0qJIuXezRHRr1AQma9o',
+                    'redirect' => 'http://localhost',
+                    'password_client' => null,
+                    'created_at' => '2017-10-22 01:34:38'
                 )
             ));
     }
